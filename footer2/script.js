a988a94de3ba4d61fc1a840a7391de820176b1f4
gulam161 <gulam.mur159@gmail.com>
2023-03-16 13:00:17 +0530

    footer2

--- footer2/script.js
+++ footer2/script.js
@@ -1,5 +1,5 @@
 let my_url = "https://orkes.io/"
 let companyLogo = document.getElementsByClassName('company-logo')
 function clickFunction(){
-    window.location.replace(my_url)
+    window.location.href =my_url
 }
